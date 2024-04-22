cfa8e98bfbc030624e5428a69ef89d7b4553eae6
Palak Sidhdhapura <pcs135@sparsh1.com>
2024-04-22 15:46:13 +0530

    apply changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var position = jQuery(window).scrollTop();
+//sticky header function
 function sticky_header(){
   var header_length = jQuery(".site-header").innerHeight();
  
@@ -8,18 +10,28 @@ jQuery("body").addClass("sticky_header");
     jQuery("body").removeClass("sticky_header");
   }
 }
+// reverse header function
+
+function reverse_header(){
+var scroll = jQuery(window).scrollTop();
+if(scroll > position){
+  jQuery("body").removeClass("nav-down").addClass("nav-up")
+}
+else{
+  jQuery("body").removeClass("nav-up").addClass("nav-down")
+}
+}
 
 
 jQuery(document).ready(function(){
-
-  // hamburger-icon js
-
+  sticky_header();
+// hamburger-icon js
   jQuery(".hamburger-icon").click(function(){
        jQuery("html,body").toggleClass("open-menu");
   });
 
-  sticky_header();
 
+// select2 js
   jQuery('.custom-dropdown').select2({
     minimumResultsForSearch: Infinity, 
     placeholder: function(){
@@ -27,10 +39,12 @@ jQuery(document).ready(function(){
   } 
   });
 
+//datepicker js
 jQuery( function() {
   jQuery( "#datepicker").datepicker();
   } );
 
+// auto complete js
 jQuery( function() {
     var availableTags = [
       "Ahemdabad",
@@ -44,7 +58,7 @@ jQuery( function() {
     });
   } );
 
-
+// video js
 jQuery('.play-btn').click(function () {
   jQuery("body,html").addClass('modal-open');
     var _this = jQuery(this).attr('data-link');
@@ -55,7 +69,7 @@ jQuery('.play-btn').click(function () {
     }, 125);
     jQuery(this).closest("body").find("video").attr('controls', true).get(0).play();
 });
-
+// custom modal js
 jQuery('.custom-modal').click(function () {
   jQuery("body,html").removeClass('modal-open');
     var _this = jQuery(this)
@@ -69,8 +83,11 @@ jQuery(".custom-modal .modal-inner-content").click(function(e){
     e.stopPropagation();
 });
 
+
+
 });
 
 jQuery(window).scroll(function(){
   sticky_header();
+  
 });
